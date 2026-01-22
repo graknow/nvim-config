4e6b3873698bb241eb43b74d62fffdf690c7694a
Grant Knowlton <knowlton.gc@outlook.com>
2026-01-22 11:11:58 -0500

    fix stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,13 +4,11 @@ local builtin = require("telescope.builtin")
 return {
     {
         "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
+        branch = "master",
         dependencies = {
             "nvim-lua/plenary.nvim",
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
         },
-        config = function()
-        end,
         keys = {
             { "<leader>ff", builtin.find_files,                                                       desc = "Find files in current directory", },
             { "<leader>fg", builtin.live_grep,                                                        desc = "Live grep on current directory file contents", },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,31 @@
 -- Highlight, edit, navigate.
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = function()
-            pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-        end,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                -- Languages
-                ensure_installed = {
-                    "c",
-                    "cpp",
-                    "javascript",
-                    "typescript",
-                    "go",
-                    "lua",
-                    "luadoc",
-                    "python",
-                    "rust",
-                    "vimdoc",
-                    "vim",
-                    "bash",
-                    "java",
-                    "html",
-                    "markdown",
-                    "regex",
-                    "tsx",
-                    "c_sharp",
-                },
-
-                highlight = { enable = true },
-                indent = { enable = true, disable = { "python" } },
-            })
-        end
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+      require("nvim-treesitter").install({
+        "c",
+        "cpp",
+        "javascript",
+        "typescript",
+        "go",
+        "lua",
+        "luadoc",
+        "python",
+        "rust",
+        "vimdoc",
+        "vim",
+        "bash",
+        "java",
+        "html",
+        "markdown",
+        "regex",
+        "tsx",
+        "c_sharp",
+        "xml",
+      })
+    end
+  },
 }
